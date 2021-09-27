607f5430598bec68ab8a15001ec6aca9e6653ce4
rudghxodid <rudghxodid@naver.com>
2021-09-27 17:33:28 +0900

    폴더명 변경

--- TaeyangLee/solopr/src/main/java/com/example/demo/repository/DaumNewsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.DaumNews;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DaumNewsRepository extends JpaRepository<DaumNews, Long> {
-    public DaumNews findByNewsNo(String newsNo);
-}
